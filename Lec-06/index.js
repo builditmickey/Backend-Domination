73a4fc6c45d88dd10b649be1859a3c39d4b5e8d1
manmindersingh227@gmail.com <manmindersingh227@gmail.com>
2024-06-14 00:21:26 +0530

    Till Lec-08

--- Lec-06/index.js
+++ Lec-06/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extented: true }));
@@ -8,13 +9,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-	res.render("index");
+	fs.readdir(`./files`, function (err, files) {
+		res.render("index", { files: files });
+	});
 });
 
-app.get("/profile/:username", function (req, res) {
-	res.send(`hey, I'm ${req.params.username}`);
+app.post("/create", function (req, res) {
+	fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+		res.redirect("/")
+	})
+
 });
 
-app.listen(3000, function () {
-	console.log("running!!");
+app.get("/file/:filename", function (req, res) {
+	fs.readFile(`./files/${req.params.filename}`, "utf-8" ,function(err,filedata){
+		res.render("show", {filename: req.params.filename, filedata : filedata})
+	})
 });
+
+app.listen(3000);
