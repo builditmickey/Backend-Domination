203bdbf090e78b8cd852ab55653242a9152e30ff
manmindersingh227@gmail.com <manmindersingh227@gmail.com>
2024-06-14 20:10:29 +0530

    Added Edit Feature

--- Lec-06/index.js
+++ Lec-06/index.js
@@ -16,15 +16,24 @@ app.get("/", function (req, res) {
 
 app.post("/create", function (req, res) {
 	fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
-		res.redirect("/")
+		res.redirect("/");
 	})
+});
 
+app.post("/edit", function (req, res) {
+	fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+		res.redirect("/");
+	})
 });
 
 app.get("/file/:filename", function (req, res) {
 	fs.readFile(`./files/${req.params.filename}`, "utf-8" ,function(err,filedata){
 		res.render("show", {filename: req.params.filename, filedata : filedata})
 	})
+});
+
+app.get("/edit/:filename", function (req, res) {
+	res.render('edit', {filename: req.params.filename})
 });
 
 app.listen(3000);
