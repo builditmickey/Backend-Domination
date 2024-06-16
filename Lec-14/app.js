7d5cbbd546463d8237b8144a0c2b5dae1849f007
manmindersingh227@gmail.com <manmindersingh227@gmail.com>
2024-06-16 21:44:39 +0530

    Cookie creation, encryption and decryption

---
+++ Lec-14/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+	let token = jwt.sign({ email: "mickey@gmail.com" }, "secret");
+	res.cookie("token", token);
+	res.send("done");
+});
+
+app.get("/read", (req, res) => {
+	let data = jwt.verify(req.cookies.token, "secret");
+	console.log(data);
+});
+
+app.listen(3000);
